25209f926f3c5d8705f651b437e8e94f82a80605
deepan <blinduck@gmail.com>
2017-01-15 12:05:04 +0800

    added static root

--- expenses/settings.py
+++ expenses/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/ubuntu/expenses-server/static/'
 
 LOGGING = {
     'version': 1,
